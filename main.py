295638cde6988be1a8d54bf59afdb0e3c8a87e42
User <talonin92@mail.ru>
2024-09-02 14:45:30 +0500

    v.step-0 molch

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import asyncio
 from A_coro import main
 
+
+def make_empty_py_files():
+    for i in range(2, 18):
+        with open(f"molchanov/step{i}.py", "w") as f:
+            f.write('')
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    make_empty_py_files()
+    # asyncio.run(main())
